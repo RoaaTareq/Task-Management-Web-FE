d918f40bcc6109321b08c726c249e7d8470a03c7
roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-09-06 00:13:40 +0300

    first vsersion

---
+++ src/Pages/Layout/NavBar.tsx
@@ -0,0 +1,24 @@
+// App.tsx or any other component where you want to use Navbar
+import React from 'react';
+import Navbar from '../../Components/Layout/NavBar'; // Adjust the path as necessary
+
+const MainNavbar: React.FC = () => {
+  const links = [
+    { href: '/home', text: 'Home' },
+    { href: '/login', text: 'Login' },
+    { href: '/register', text: 'Register' }
+  ];
+
+  return (
+    <div>
+      <Navbar
+        brandName="Task Mangament Tracker"
+        links={links}
+        className="custom-navbar-class" // Optional
+      />
+      {/* Other components and content */}
+    </div>
+  );
+};
+
+export default MainNavbar;
