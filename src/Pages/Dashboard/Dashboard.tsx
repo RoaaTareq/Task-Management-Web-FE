d25368ea899f51a3bad7d377fd4abae2cd9619ed
roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-09-07 00:33:01 +0300

    Upload new version

---
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const Dashboard: React.FC = () => {
+  return (
+    <section>
+        <div className="container">
+            
+        </div>
+    </section>
+  );
+};
+
+export default Dashboard
