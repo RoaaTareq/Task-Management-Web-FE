0014159852b604fb9ea579feea6fe843618d88e6
roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-09-08 21:58:24 +0300

    enhance code

--- src/Pages/Dashboard/User/UserList.tsx
+++ src/Pages/Dashboard/User/UserList.tsx
@@ -1,20 +1,17 @@
-import React from 'react'
-import Button from '../../../Components/Buttons/Button'
-import ViewUser from './ViewUser'
-
+import React from "react";
+import Button from "../../../Components/Buttons/Button";
+import ViewUser from "./ViewUser";
 
 const UserList: React.FC = () => {
-  return (
-    <section>
-        <div className="container">
-           
-            <h1>Users</h1>
-           
-           
-            <ViewUser/>
-        </div>
-    </section>
-  );
+    return (
+        <section className="mt-4">
+            <div className="container">
+                <h1>Users</h1>
+
+                <ViewUser />
+            </div>
+        </section>
+    );
 };
 
 export default UserList;
