cbe09d4d4086f5a65bd43399485d370971476578
roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-09-09 18:50:29 +0300

    private route

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainNavbar from './Pages/Layout/NavBar'; // Adjust path if necessary
+import MainNavbar from './Pages/Layout/NavBar'; 
 import MobileNavBar from './Pages/Auth/MobileNavBar';
 import Home from './Pages/Home';
 import Login from './Pages/Auth/Login';
 import Register from './Pages/Auth/Register';
-import Logout from './Pages/Auth/Logout'
-import './App.css'; // Import your CSS file
+import Logout from './Pages/Auth/Logout';
+import './App.css'; 
 import Dashboard from './Pages/Dashboard/Dashboard';
-import AuthProvider from './Context/AuthContext'; // Import AuthProvider
+import AuthProvider from './Context/AuthContext'; 
+import ProtectedRoute from './Components/privateRoute'; 
 
-function App() {
+const App: React.FC = () => {
   return (
     <AuthProvider>
       <Router>
-      <div className="main-navbar">
+        <div className="main-navbar">
           <MainNavbar />
         </div>
         <div className="mobile-navbar">
@@ -25,7 +26,16 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/logout" element={<Logout />} />
-          <Route path="/*" element={<Dashboard />} />
+          
+          {/* Protect the Dashboard and its routes */}
+          <Route 
+            path="/*" 
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            } 
+          />
         </Routes>
       </Router>
     </AuthProvider>
